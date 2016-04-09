fc56831a669633981fe345d678a263d8d5facefb
JohanX <johan@johan.ro>
2016-04-09 10:16:43 +0300

    fix line endings. add cors

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var corsMiddleware = require('restify-cors-middleware');
 module.exports = {
     port: 8000,
     middleware: [
@@ -7,6 +7,15 @@ module.exports = {
         'dateParser',
         'queryParser',
         'bodyParser',
+        function cors(mycro) {
+            var cors = corsMiddleware({
+                preflightMaxAge: 5,
+                origins: ['*'],
+                credentials: true // defaults to false
+            });
+            mycro.server.pre(cors.preflight);
+            return cors.actual;
+        },
         'morgan',
         'request',
         'request-all-params',
